f6cc6a8e444ac3718b3e9086c2faf743ecf08a04
Flames Elite <rinagunnerx@gmail.com>
2023-06-05 16:58:21 -0700

    Add files via upload

---
+++ SMW[ROM-MANAGER-V0.0.FT].py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class LunarMagicClone:
+    def __init__(self):
+        """
+        Initializes the LunarMagicClone application.
+        """
+        self.root = tk.Tk()
+        self.root.geometry('600x400')
+        self.root.title('@SMW ROM MANAGER 0.X.A AI v0.0.1')
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    # Create and run the LunarMagicClone application
+    LunarMagicClone()
+
+    ## @[Flames Co. Systems - 20XX @[C]]
